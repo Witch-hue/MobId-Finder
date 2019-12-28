d43e77add7c7ec7751403f6e5d0aa063e066f46d
Witch <59118604+Witch-hue@users.noreply.github.com>
2019-12-27 22:33:41 -0600

    Find MobID's by killing monster in question
    Patch 88 Working,

---
+++ README.md
@@ -0,0 +1,17 @@
+Original Author Seren, Updated and upkept by Witch
+
+## Commands
+
+/8 mobid | Toggle module on/off - to stop spamming your console. (Enabled by Default)
+
+/8 mobidsysmsg | Toggle the MobID's from showing in-game chat. (Disabled by Default)
+
+
+# Mob-id-finder
+Easy way to find a mob's huntingZoneId and templateId by killing the mob
+
+Requires Commands module: https://github.com/tera-toolbox/command
+
+
+## Instruction
+After making sure this module is enabled, load the target mob onto your screen and then kill it (strongly suggest you only hit that target mob). The killed mob huntingZoneId and templateId ids will be displayed on the console (command prompt) by default.
---
+++ index.js
@@ -0,0 +1,37 @@
+//Defaults
+let sysmsg=false, //display system messages for ids (Default false)
+	enabled=true //default enable/disable module
+
+module.exports = function mobidfinder(dispatch) {
+
+	let idlist=new Map()
+
+	dispatch.command.add('mobid', () => {
+			enabled = !enabled
+			if(!enabled) idlist.clear()
+			dispatch.command.message(enabled ? 'Mob ID finder Enabled. Kill the mob and ids should be displayed.' : 'Mob ID finder Disabled')
+	})
+
+	dispatch.command.add('mobidsysmsg', () => {
+			sysmsg = !sysmsg
+			dispatch.command.message(`Mob ID sysmsg ${sysmsg ? 'enabled' :'disabled'}`)
+	})
+
+	dispatch.hook('S_SPAWN_NPC', 11, event => {
+		if(enabled) idlist.set(event.gameId,`${event.huntingZoneId}_${event.templateId}`);
+	})
+
+	dispatch.hook('S_DESPAWN_NPC', 3,event => {
+		if(enabled && event.type===5) {
+			if(idlist.has(event.gameId)) {
+				console.log(`Monster id (huntingZoneId_templateId): ${idlist.get(event.gameId)}`)
+				if(sysmsg) dispatch.command.message(`Monster id (huntingZoneId_templateId): ${idlist.get(event.gameId)}`)
+				idlist.delete(event.gameId)
+			}
+		}
+	})
+
+	dispatch.hook('S_LOAD_TOPO', "raw",event => {
+		idlist.clear()
+	})
+}
